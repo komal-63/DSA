c0892888eedc0416e6a5f89ce119b31a786566e6
manjeet <thebloghub.001@gmail.com>
2026-08-05 00:28:54 +0530

    moves zero at end

--- Array/movezerosatend.php
+++ Array/movezerosatend.php
@@ -1,5 +1,5 @@
 <?php
-$array= [1,3,0,4,0,6];
+$array = [1,3,0,4,0,6];
 
 $left=0;
 $right=0;
@@ -10,6 +10,7 @@ for($right=0; $right<$length; $right++){
     if($array[$right]!=0){
         $temp=$array[$left];
         $array[$left]=$array[$right];
+        $array[$right]=$temp;
         $left++;
     }
    
